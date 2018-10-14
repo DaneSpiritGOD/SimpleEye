e7486a811cca6b9994cd4241062615a9ebc84749
Dane <838652739@qq.com>
2018-10-14 15:25:41 +0800

    添加了时间控件

--- DisplayCenter/Views/MainWindow.xaml.cs
+++ DisplayCenter/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DisplayCenter.ViewModels;
+using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
-using DisplayCenter.ViewModels;
+using System.Windows.Threading;
 
 namespace DisplayCenter.Views
 {
@@ -23,9 +24,19 @@ namespace DisplayCenter.Views
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private readonly DispatcherTimer _timer = new DispatcherTimer(DispatcherPriority.ApplicationIdle);
         public MainWindow()
         {
-            InitializeComponent(); 
+            InitializeComponent();
+
+            _timer.Tick += Timer_Click;
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Start();
+        }
+
+        private void Timer_Click(object sender, EventArgs e)
+        {
+            timeText.Text = DateTime.Now.ToString(string.Format("yy{0}MM{1}dd{2}HH{3}mm{4}ss", "/", "/", " ", ":", ":"));
         }
     }
 }
